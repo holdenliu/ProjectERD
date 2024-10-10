2262caa0059c21d3ee39d40ad01c252d6c4c96a1
Holden Liu <holdenliu@gmail.com>
2024-10-10 13:18:24 -0700

    ball changes

--- Assets/Steps/MiniGames/BallRoll/ballroll.cs
+++ Assets/Steps/MiniGames/BallRoll/ballroll.cs
@@ -8,6 +8,7 @@ public class ballroll : MonoBehaviour
     public float moveSpeed;
     public float jumpForce;
     public float gravityScale;
+    public float maxAngularVelocity;
     public Transform camera;
 
     private Vector3 moveDirection;
@@ -16,18 +17,24 @@ public class ballroll : MonoBehaviour
     void Start()
     {
         theRB = GetComponent<Rigidbody>();
-        // Remove CharacterController-related code
+        Cursor.lockState = CursorLockMode.Locked;
+        theRB.maxAngularVelocity = maxAngularVelocity;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         // Capture movement input relative to the camera's orientation
-        moveDirection = (camera.forward * Input.GetAxis("Vertical")) + (camera.right * Input.GetAxis("Horizontal"));
-        moveDirection = moveDirection.normalized * moveSpeed;
-        theRB.AddForce(Physics.gravity * gravityScale);
-        // Add force for horizontal movement (on x and z axes)
-        theRB.AddForce(new Vector3(moveDirection.x, 0f, moveDirection.z), ForceMode.Force);
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            moveDirection = (camera.forward * Input.GetAxis("Vertical")) + (camera.right * Input.GetAxis("Horizontal"));
+            moveDirection = moveDirection.normalized * moveSpeed;
+            theRB.AddForce(Physics.gravity * gravityScale);
+            // Add force for horizontal movement (on x and z axes)
+            theRB.AddForce(new Vector3(moveDirection.x, 0f, moveDirection.z), ForceMode.Force);
+        }
+            
 
         
 
