a139a1fee0be86b70eec99184895ebbc91d852b2
Holden Liu <holdenliu@gmail.com>
2024-10-11 16:09:44 -0700

    change

--- Assets/test.cs
+++ Assets/test.cs
@@ -5,4 +5,5 @@ using UnityEngine;
 public class test : MonoBehaviour
 {
     //https://forms.office.com/Pages/DesignPageV2.aspx?prevorigin=Marketing&origin=NeoPortalPage&subpage=design&id=SHJZBzjqG0WKvqY47dusgW80ra3elpZDsWdc8Cz5fsxUNTVGRjNKMEVDR1NLMDJQMkNKNThKSUM3MC4u
+    https://forms.office.com/Pages/ResponsePage.aspx?id=SHJZBzjqG0WKvqY47dusgW80ra3elpZDsWdc8Cz5fsxUNTVGRjNKMEVDR1NLMDJQMkNKNThKSUM3MC4u
 }
